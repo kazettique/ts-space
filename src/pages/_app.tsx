c80baad24f42f1a125c0d7ae610ff08acdf313e4
Woody <kazettique@gmail.com>
2021-05-24 22:30:05 +0800

    Change app component name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import '@/styles/global.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
