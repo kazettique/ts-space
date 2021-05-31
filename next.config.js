f10abcc5d77498df7b65399e3b27298d388d985d
Woody <kazettique@gmail.com>
2021-05-31 21:53:37 +0800

    Set i18n & install tailwindCSS

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
+const nextTranslate = require('next-translate');
+
 module.exports = {
-  future: {
-    webpack5: true,
-  },
+  ...nextTranslate(),
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
+// next
 import { AppProps } from 'next/app';
+import Image from 'next/image';
 import '@/styles/global.css';
+import Head from 'next/head';
+
+// tailwind
+import 'tailwindcss/tailwind.css';
+
+// component
+import Menu from '@/components/Menu';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>TS Space</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Menu />
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
