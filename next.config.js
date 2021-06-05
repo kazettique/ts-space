fc3551448c71a99dd4105407d22e1b80f83d6139
Woody <kazettique@gmail.com>
2021-06-06 00:14:56 +0800

    Deploy config test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const nextTranslate = require('next-translate');
 
+const debug = process.env.NODE_ENV !== 'production';
+
 module.exports = {
   ...nextTranslate(),
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  basePath: debug ? '' : process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: debug ? '' : process.env.NEXT_PUBLIC_BASE_PATH,
 };
